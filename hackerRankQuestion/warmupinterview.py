05f64787d9d84467b3855b1a6a0a8b2ed546262e
solomonugo <kingsolodawarrior@gmail.com>
2022-02-20 23:25:06 +0100

    Answer now works for all cases

--- hackerRankQuestion/warmupinterview.py
+++ hackerRankQuestion/warmupinterview.py
@@ -1,16 +1,19 @@
 def jumpingOnClouds(c):
     count = 0
     i = 0
-    while i < len(c) - 1:
-        if c[i + 2] == 1 or i + 2 >= len(c):
-            i += 1
-            count += 1 
-        else:
+    while i < len(c) - 2:
+        if c[i + 2] == 0:
             i += 2
-            count += 1
+        else:
+            i += 1
+        count += 1
+
+    if i != len(c) - 1:
+        count += 1
     return count
 
 if __name__=='__main__':
     c = [0, 1, 0, 0, 0, 1, 0]
+    n = [0, 0, 0, 1, 0, 0]
 
-    print(jumpingOnClouds(c))
+    print(jumpingOnClouds(n))
